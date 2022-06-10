aeda210de74679e2d0699e141d6815d99247767d
Jessy Baldelli <jessybaldelli@gmail.com>
2022-06-10 12:53:25 +0200

    maj bootstrap & jquery javascript

--- js/map.js
+++ js/map.js
@@ -1,10 +1,34 @@
-let map;
-
+// On initialise la latitude et la longitude de Paris (centre de la carte)
+const lat = 4.82922464978;
+const lon = 45.7699284397;
+let map = null;
+// Fonction d'initialisation de la carte
 function initMap() {
+  // Créer l'objet "map" et l'insèrer dans l'élément HTML qui a l'ID "map"
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
+    // Nous plaçons le centre de la carte avec les coordonnées ci-dessus
+    center: new google.maps.LatLng(lat, lon), 
+    // Nous définissons le zoom par défaut
+    zoom: 11, 
+    // Nous définissons le type de carte (ici carte routière)
+    mapTypeId: google.maps.MapTypeId.ROADMAP, 
+    // Nous activons les options de contrôle de la carte (plan, satellite...)
+    mapTypeControl: true,
+    // Nous désactivons la roulette de souris
+    scrollwheel: false, 
+    mapTypeControlOptions: {
+      // Cette option sert à définir comment les options se placent
+      style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR 
+    },
+    // Activation des options de navigation dans la carte (zoom...)
+    navigationControl: true, 
+    navigationControlOptions: {
+      // Comment ces options doivent-elles s'afficher
+      style: google.maps.NavigationControlStyle.ZOOM_PAN 
+    }
   });
 }
-
-window.initMap = initMap;
+window.onload = function(){
+  // Fonction d'initialisation qui s'exécute lorsque le DOM est chargé
+  initMap(); 
+};
